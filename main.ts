be9600ba6ed041457279dc92fa7ef366eb6a0322
Pepetoke <134915347+Pepetoke@users.noreply.github.com>
2023-06-08 09:16:04 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
-    basic.showString("ºC")
+    basic.showString("C")
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(input.temperature() * (9 / 5) + 32)
-    basic.showString("ºF")
+    basic.showString("F")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(input.temperature() + 273.15)
-    basic.showString("ºK")
+    basic.showString("K")
 })
